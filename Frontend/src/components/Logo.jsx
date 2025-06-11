7e686ffadd2d16531f601424affbf2cf197bd1a5
Priyanshu <chauhanpriyanshu3131@gmail.com>
2025-06-11 17:56:52 +0530

    Resolving Frontend Errors

---
+++ Frontend/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width='100px'}) {
+  return (
+    <div className='bg-b'>
+    <img src="https://images.pexels.com/photos/593158/pexels-photo-593158.jpeg?cs=srgb&dl=pexels-scottwebb-593158.jpg&fm=jpg" alt="" className="w-12 h-12 rounded-lg" />
+    </div>
+  )
+}
+
+export default Logo 

d8d5207bd8aac73a091a35a1e50b05b94f21198e
Priyanshu <chauhanpriyanshu3131@gmail.com>
2025-06-30 02:05:55 +0530

    Update

--- Frontend/src/components/Logo.jsx
+++ Frontend/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width='500px'}) {
   return (
     <div className='bg-b '>
-    <img src="https://sdmntprnorthcentralus.oaiusercontent.com/files/00000000-42d8-622f-ab7e-6af07702e498/raw?se=2025-06-29T19%3A54%3A01Z&sp=r&sv=2024-08-04&sr=b&scid=33fdc18a-71f4-5a33-be7c-9f34a03acd78&skoid=bbd22fc4-f881-4ea4-b2f3-c12033cf6a8b&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-06-29T18%3A53%3A22Z&ske=2025-06-30T18%3A53%3A22Z&sks=b&skv=2024-08-04&sig=9f3nJ1U8mZrW58weXObuQov9dbqst2O%2BGuDPdC2fbho%3D" alt="Logo" className="w-20 h-21 rounded-lg" />
+    <img src="https://www.shutterstock.com/image-vector/pencil-flat-icon-single-high-260nw-737037337.jpg" alt="Logo" className="w-18 h-16 rounded-lg" />
     </div>
   )
 }
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:7000/api/v1",
+  // baseURL: "http://localhost:7000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true, // important for sending cookies (auth)
 });
 
